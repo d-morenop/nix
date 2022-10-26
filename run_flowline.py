ec29d112edc45e89517069d8da5c71934f012ad5
Daniel <dmoren07@ucm.es>
2022-10-26 16:42:40 +0200

    Stable version of run_flowline.py in Brigit to submit jobs in queue.

--- run_flowline.py
+++ run_flowline.py
@@ -13,7 +13,6 @@ import shutil
 from subprocess import Popen, PIPE, STDOUT
 
 
-
 # PYTHON SCRIPT TO COMPILE AND RUN THE FLOWLINE MODEL.
 # This script copies flowline.cpp and its dependencies to a desired folder
 # where it is compiled and run. The simulation output is then saved in a .nc file
@@ -24,8 +23,8 @@ from subprocess import Popen, PIPE, STDOUT
 
 
 # User defined directories.
-path_flowline = "/home/dmoreno/c++/git/flowline/"
-path_output   = "/home/dmoreno/c++/flowline/output/mismip/exp3/exp3_n.250_long/"
+path_flowline = "/home/dmoren07/c++/git/flowline/"
+path_output   = "/home/dmoren07/c++/flowline/output/mismip/exp3/test2.sbatch/"
 
 
 # Create new directory if not existing. Make clean otherwise.
@@ -40,10 +39,14 @@ else:
     print('')
     print('-> Existing directory. Make clean.')
     print('')
+
+    # Boolean to check if path exists.
     isfile = os.path.isdir(path_output+'*.o')
+    
+    # Make clean if path does exist.
     if isfile == True:
         subprocess.run("rm "+path_output+"*.o", shell=True, check=True, \
-                           stdout=subprocess.PIPE, universal_newlines=True)
+                       stdout=subprocess.PIPE, universal_newlines=True)
     
 
 # Copy main script and write_nc to the output folder for compilation therein.
@@ -52,39 +55,62 @@ shutil.copyfile(path_flowline+'write_nc.cpp', path_output+'write_nc.cpp')
 
 
 # Compilation configuration. ['local', 'iceberg', 'brigit']
-config = 'iceberg'
+config = 'brigit'
 
 if config == 'local':
+    
+    # Compiling command.
     cmd  = "g++ -I /path/to/eigen3/ -o "+path_output+"flow_line.o "+path_output+"flow_line.cpp -lnetcdf"
-elif config == 'iceberg':
-    cmd = "g++ -std=c++11 -I /usr/include/eigen3/ -o "+path_output+"flow_line_mismip_4.o "+path_output+"flow_line_mismip_4.cpp -lnetcdf"
-elif config == 'brigit':
-    cmd = "g++ -std=c++11 -I/opt/ohpc/pub/libs/gnu8/impi/netcdf/4.6.3/include/ -I/usr/include/eigen3/ -L/opt/ohpc/pub/libs/gnu8/impi/netcdf/4.6.3/lib/ -lnetcdf -o "+path_output+"flow_line.o "+path_output+"flow_line.cpp"
 
+elif config == 'iceberg':
+    
+    # Compiling command.
+    cmd = "g++ -std=c++11 -I /usr/include/eigen3/ -o "+path_output+"flow_line_mismip_4.o "+path_output+"flow_line_mismip_4.cpp -lnetcdf"
+
+elif config == 'brigit':
+    
+    # Compiling command.
+    cmd = "g++ -std=c++11 -I/opt/ohpc/pub/libs/gnu8/impi/netcdf/4.6.3/include/ -I/usr/include/eigen3/ -L/opt/ohpc/pub/libs/gnu8/impi/netcdf/4.6.3/lib/ -lnetcdf -o "+path_output+"flow_line_mismip_4.o "+path_output+"flow_line_mismip_4.cpp"
+    
+    # In Brigit, we need a submit.sh file to send job to the queue.
+    shutil.copyfile(path_flowline+'submit.sh', path_output+'submit.sh')
 
 # Create text file for terminal output. 
 f = open(path_output+"out.txt", "w")
 
+# Print compiling config.
 print('')
 print('-> Compiling configuration: ', config)
 print('')
 
 # Compile flowline with subprocess.
 subprocess.run(cmd, shell=True, check=True, \
-                   stdout=f, universal_newlines=True)
+               stdout=f, universal_newlines=True)
 
 print('')
 print('-> Flowline compiled.')
 print('')
 
 print('')
-print('-> Running flowline:', path_output)
+print('-> Running flowline.')
 print('')
     
 # Run flowline in background. Note that the solution is stored in nc file.
-p = subprocess.Popen(path_output+"flow_line_mismip_4.o &", shell=True, \
-                         stdout=f, universal_newlines=True)
+# In Brigit, we need submit.sh to send it to the queue.
+if config == 'brigit':
 
+    # Old version
+    #cmd_run = "sbatch "+path_output+"submit.sh"
 
+    # Try changing working directory and then running sbatch there.
+    os.chdir(path_output)
+    cmd_run = "sbatch submit.sh"
+    print('cmd_run = ', cmd_run)
+else:
+    cmd_run = path_output+"flow_line_mismip_4.o &"
+
+# Run flowline model.
+p = subprocess.Popen(cmd_run, shell=True, \
+                     stdout=f, universal_newlines=True)
 
 
